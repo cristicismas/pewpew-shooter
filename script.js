42bcf8443b324038f1437b68b4c8c68cf7a4e504
Cristi Cismas <cristicismas99@gmail.com>
2019-03-05 16:56:14 +0200

    Log move keys on keypress

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-$(document).ready(() => {
-  const bgMusic = document.getElementById('background-music');
+const W = 87;
+const A = 65;
+const S = 83;
+const D = 68;
 
+const up = 38;
+const left = 37;
+const down = 40;
+const right = 39;
+
+$(document).ready(() => {
+  // Play background music.
+  const bgMusic = document.getElementById('background-music');
   bgMusic.play();
+
+  // Render ship.
+  $('#space-craft').prepend('<img src="./assets/spacecraft/spaceCraft1_N.png" />');
+
+  // Listen for keypress
+  $(document).keydown(function(e) {
+    switch(e.keyCode) {
+      case W:
+      case up:
+        console.log('up');
+        break;
+      case A:
+      case left:
+        console.log('left');
+        break;
+      case S:
+      case down:
+        console.log('down');
+        break;
+      case D:
+      case right:
+        console.log('right');
+        break;
+    }
+  });
 });
+
+function startGame() {
+  $('#welcome-message').hide();
+  $('#start-btn').hide();
+}
