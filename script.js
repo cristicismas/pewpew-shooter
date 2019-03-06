49df43cfe49fb070396abd5ee9ed410cb4159395
Cristi Cismas <cristicismas99@gmail.com>
2019-03-06 18:51:15 +0200

    Add functionality to handle ship movement

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const A = 65;
 const S = 83;
 const D = 68;
 
-const up = 38;
-const left = 37;
-const down = 40;
-const right = 39;
+const upKey = 38;
+const leftKey = 37;
+const downKey = 40;
+const rightKey = 39;
 
 const MAX_STARS = 250;
 
@@ -33,27 +33,20 @@ $(document).ready(() => {
   // Render ship.
   $('#space-craft').prepend('<img src="./assets/spacecraft/spaceCraft1_N.png" />');
 
-  // Listen for keypress
+  // Handle ship movement
+  var keysPressed = {};
+
   $(document).keydown(function(e) {
-    switch (e.keyCode) {
-      case W:
-      case up:
-        console.log('up');
-        break;
-      case A:
-      case left:
-        console.log('left');
-        break;
-      case S:
-      case down:
-        console.log('down');
-        break;
-      case D:
-      case right:
-        console.log('right');
-        break;
-    }
+    keysPressed[e.keyCode] = true;
   });
+
+  $(document).keyup(function(e) {
+    delete keysPressed[e.keyCode];
+  });
+
+  setInterval(function() {
+    moveShip(canvas, keysPressed);
+  }, 20);
 });
 
 function generateStars(canvas) {
@@ -92,4 +85,26 @@ function getRandomNumber(max) {
 function startGame() {
   $('#welcome-message').hide();
   $('#start-btn').hide();
+}
+
+function moveShip(canvas, keysPressed) {
+  const { left, top } = $('#space-craft').offset();
+
+  for (var key in keysPressed) {
+    if (!keysPressed.hasOwnProperty(key)) continue;
+    if (!document.hasFocus()) continue;
+
+    if (key == leftKey || (key == A && left >= -40)) {
+      $('#space-craft').animate({ left: '-=10' }, 0);
+    }
+    if (key == upKey || (key == W && top >= 0)) {
+      $('#space-craft').animate({ top: '-=10' }, 0);
+    }
+    if (key == rightKey || (key == D && left < canvas.width - 90)) {
+      $('#space-craft').animate({ left: '+=10' }, 0);
+    }
+    if (key == downKey || (key == S && top < canvas.height - 100)) {
+      $('#space-craft').animate({ top: '+=10' }, 0);
+    }
+  }
 }
