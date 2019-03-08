e844d803c0aa9ca136244983d2a5505984758047
Cristi Cismas <cristicismas99@gmail.com>
2019-03-08 16:32:42 +0200

    Indent functions in spacecraft.js correctly

--- spacecraft.js
+++ spacecraft.js
@@ -13,24 +13,24 @@ $(document).keyup(function(e) {
 
 export function moveShip(canvas) {
   setInterval(function() {
-  const { left, top } = $('#space-craft').offset();
+    const { left, top } = $('#space-craft').offset();
 
-  for (var key in keysPressed) {
-    if (!keysPressed.hasOwnProperty(key)) continue;
-    if (!document.hasFocus()) continue;
+    for (var key in keysPressed) {
+      if (!keysPressed.hasOwnProperty(key)) continue;
+      if (!document.hasFocus()) continue;
 
-    if (key == KEYS.leftKey || key == KEYS.A && left >= -40) {
-      $('#space-craft').animate({ left: '-=10' }, 0);
+      if (key == KEYS.leftKey || key == KEYS.A && left >= -40) {
+        $('#space-craft').animate({ left: '-=10' }, 0);
+      }
+      if (key == KEYS.upKey || key == KEYS.W && top >= 0) {
+        $('#space-craft').animate({ top: '-=10' }, 0);
+      }
+      if (key == KEYS.rightKey || key == KEYS.D && left < canvas.width - 90) {
+        $('#space-craft').animate({ left: '+=10' }, 0);
+      }
+      if (key == KEYS.downKey || key == KEYS.S && top < canvas.height - 100) {
+        $('#space-craft').animate({ top: '+=10' }, 0);
+      }
     }
-    if (key == KEYS.upKey || key == KEYS.W && top >= 0) {
-      $('#space-craft').animate({ top: '-=10' }, 0);
-    }
-    if (key == KEYS.rightKey || key == KEYS.D && left < canvas.width - 90) {
-      $('#space-craft').animate({ left: '+=10' }, 0);
-    }
-    if (key == KEYS.downKey || key == KEYS.S && top < canvas.height - 100) {
-      $('#space-craft').animate({ top: '+=10' }, 0);
-    }
-  }
   }, 20);
 }
