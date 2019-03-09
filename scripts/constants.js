5f6005be39e47b9ce9fa28bd1add060aa597faea
Cristi Cismas <cristicismas99@gmail.com>
2019-03-09 23:14:36 +0200

    Add urls to enemy ships in constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -21,18 +21,21 @@ export const SHIP = {
 
 export const ENEMY = {
   E1: {
+    URL: './assets/enemies/enemy-1.png',
     WIDTH: 35,
     HEIGHT: 50,
     MOVE_DELAY: 20,
     SPEED: 3
   },
   E2: {
+    URL: './assets/enemies/enemy-2.png',
     WIDTH: 80,
     HEIGHT: 60,
     MOVE_DELAY: 25,
     SPEED: 3
   },
   E3: {
+    URL: './assets/enemies/enemy-3.png',
     WIDTH: 100,
     HEIGHT: 80,
     MOVE_DELAY: 30,
