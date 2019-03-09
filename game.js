04eb774ad8b1b973ad8623743609af85e67d66b0
Cristi Cismas <cristicismas99@gmail.com>
2019-03-09 17:08:54 +0200

    Handle movement and shooting only when game starts

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
+import { handleMovement, handleShooting } from './spacecraft.js';
+
 export function handleStartGame() {
   $('#start-btn').click(() => {
+    handleMovement();
+    handleShooting();
+
     $('#welcome-message').fadeOut('slow');
     $('#start-btn').fadeOut('slow');
   });
--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-import { handleMovement, handleShooting } from './spacecraft.js';
 import { renderBackground } from './background.js';
 import { handleStartGame } from './game.js';
 
 $(document).ready(() => {
   renderBackground();
   handleStartGame();
-  handleMovement();
-  handleShooting();
 });
